37b5529b16f064b26e008573eaa8cd95cbd980bc
emeitiai <matinunezmora@gmail.com>
2023-01-19 21:18:36 -0300

    feat: :tada: first commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_800ExtraBold
+} from '@expo-google-fonts/poppins'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Poppins_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-2xl" style={{ fontFamily: 'Poppins_800ExtraBold' }}>
+          Pokédex
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,5 @@
+# Pokédex | React Native and NativeWind (TailwindCSS)
+
+![hola](https://media.tenor.com/zWJN3FozxQcAAAAC/surprised-pikachu-surprised.gif)
+
+- inspired by: https://www.figma.com/file/3wPkxbq9gBOOU65RBZ2B02/Pok%C3%A9dex
