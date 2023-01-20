94a5edc2e89bb04df25ff5332728d694bade1c82
emeitiai <matinunezmora@gmail.com>
2023-01-20 11:47:06 -0300

    feat: :alien: conexión a la pokeapi

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList  } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import PokemonCard from './components/PokemonCard';
+import axios from 'axios';
 import {
   useFonts,
   Poppins_800ExtraBold,
@@ -9,6 +11,29 @@ import {
 } from '@expo-google-fonts/poppins'
 
 export default function App() {
+  const [pokemones, setPokemones] = useState({})
+
+  useEffect(() => {
+    const getDataUsingAsyncAwaitGetCall = async () => {
+      try {
+        const response = await axios.get(
+          'https://pokeapi.co/api/v2/pokedex/2/',
+        );
+        const { pokemon_entries } = await response.data
+        setPokemones(Object.values(pokemon_entries))
+
+      } catch (error) {
+        // handle error
+        alert(error.message);
+      }
+    };    
+    getDataUsingAsyncAwaitGetCall()
+    console.log(pokemones)
+  }, [])
+
+
+  // console.log(pokemones.length || 'cargando')
+
   let [fontsLoaded] = useFonts({
     Poppins_800ExtraBold,
     Poppins_400Regular
@@ -22,6 +47,11 @@ export default function App() {
           Pokédex
         </Text>
         <PokemonCard/>
+        <FlatList
+          data={pokemones}
+          renderItem={({item}) => <Text>{item.pokemon_species.name}</Text>}
+          keyExtractor={item => item.entry_number}
+        />
         <StatusBar style="auto" />
       </View>
     );
